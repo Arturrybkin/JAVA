51a1364302629931f9a64bb754814d4fc46855d4
artur rybkin <aryr.rybkin.03@mail.ru>
2022-12-01 00:28:23 +0300

    01.12

---
+++ labs/lab1/Palindrome.java
@@ -0,0 +1,24 @@
+package JAVA.labs.lab1;
+
+public class Palindrome {
+
+
+    public static void main(String[] args) {
+        boolean isAllPalindrome = true;
+        for (int i = 0; i < args.length; i++) {
+            if (!isPalindrome(args[i])) {
+                isAllPalindrome = false;
+            }
+            System.out.println(args[i]+" "+isAllPalindrome);
+        }
+    }
+
+    public static String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    private static boolean isPalindrome(String s) {
+        return s.equals(reverseString(s));
+    }
+    
+}
